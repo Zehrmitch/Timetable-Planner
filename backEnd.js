5929f2e21e3dfdea83106183e9e1583a94f5fb27
Mitchell Zehr <zehrmitch@gmail.com>
2020-11-03 16:04:49 -0500

    Create and show schedules
    Add the ability to show and update schedules

--- backEnd.js
+++ backEnd.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const fs = require("fs");
 const process = require("process");
+var flatCache = require('flat-cache');
 const app = express();
 const port = process.env.PORT || 3000;
 let rawdata = fs.readFileSync("data.json");
 var text = JSON.parse(rawdata);
-var http = require("http");
 var router = express.Router();
+var cache = flatCache.load('scheduleCache');
 
 // REST
 app.use(express.json());
@@ -17,23 +18,103 @@ router.get("/nameAndCodes", function(req, res) {
 	res.send(nameAndCodesArray);
 });
 
-app.get("/api/getCourseCodes/:id", (req, res) => {
+router.get("/getCourseCodes/:id", function(req, res) {
 	let nameAndCodesArray = getCourseCodes(req.params.id);
 	res.send(nameAndCodesArray);
 });
 
-app.get("/api/getCourseSearch/:SC/:CC/:OC", (req, res) => {
+router.get("/getCourseSearch/:SC/:CC/:OC", function(req, res) {
 	let courseSearch = getCourseSearch(req.params.SC, req.params.CC, req.params.OC);
 	res.send(courseSearch);
+});
+
+router.get("/getSchedule/:scheduleName", function(req, res) {
+    let scheduleSearch = getSchedule(req.params.scheduleName);
+	res.send(scheduleSearch);
+});
+
+router.get("/viewSchedules", function(req, res) {
+    let allSchedules = getSchedules();
+	res.send(allSchedules);
+});
+
+router.post("/createSchedule/:SN", function(req, res) {
+    let newSchedule = createSchedule(req.params.SN);
+	res.send(newSchedule);
+});
+
+router.put("/updateSchedule/:scheduleName", function(req, res) {
+    let updatedSchedule = updateSchedule(req.params.scheduleName, req.body);
+	res.send(updatedSchedule);
+});
+
+router.delete("/deleteSchedule/:scheduleName", function(req, res) {
+    let deleteASchedule = deleteSchedule(req.params.scheduleName);
+	res.send(deleteASchedule);
+});
+
+router.delete("/deleteAllSchedules", function(req, res) {
+    let deleteAllSchedule = deleteAllSchedules();
+	res.send(deleteAllSchedule);
 });
 
 app.use('/api', router);
 
 app.listen(port, () => {
-	console.log(`Timetable is listening at http://localhost:${port}`);
+    console.log(`Timetable is listening at http://localhost:${port}`);
 });
 
 // Functions
+
+function updateSchedule(sName, sCourses) {
+    cache.setKey(sName, sCourses);
+    cache.save(true);
+    let schedule = cache.getKey(sName);
+    return schedule;
+}
+
+function getSchedules(){
+    let allSchedules = cache.all();
+    console.log(allSchedules);
+    return allSchedules;
+}
+
+function getSchedule(sName){
+    let schedule = cache.getKey(sName);
+    console.log(schedule);
+    return schedule;
+}
+
+function deleteSchedule(sName) {
+    cache.removeKey(sName);
+    cache.save(true);
+    let scheduleDelete = "removed";
+    return scheduleDelete;
+}
+
+function deleteAllSchedules() {
+    let allSchedule = cache.all();
+    for (var key in allSchedule){
+        cache.removeKey(key);
+    }
+    cache.save(true);
+    return allSchedule;
+}
+
+function createSchedule(sName) {
+    if(cache.getKey(sName) == undefined){
+        cache.setKey(sName, { code: 'subject'});
+        cache.save(true);
+        let schedule = cache.getKey(sName);
+        console.log(schedule);
+        return schedule;
+    }
+    else{
+        let resposeToCreate = { info: 'Schedule already exists' };
+        return resposeToCreate;
+    }
+}
+
 function getNameAndCode() {
 	var nameAndCodes = text.map(function (text) {
         var content = { subject: text.subject, className: text.className };
@@ -59,7 +140,7 @@ function getCourseCodes(SC) {
 
 function getCourseSearch(SC, CC, OC) {
     var subjectCodes = text.filter(function (text) {
-        if (SC === text.subject && CC === text.catalog_nbr){
+        if (SC === text.subject && String(CC) === String(text.catalog_nbr)){
             return text;
         }
     });
--- static/frontEnd.js
+++ static/frontEnd.js
@@ -1,3 +1,16 @@
+function sanitize(string) {
+    const map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#x27;',
+        "/": '&#x2F;',
+    };
+    const reg = /[&<>"'/]/ig;
+    return string.replace(reg, (match)=>(map[match]));
+}
+
 function createNode(element) {
 	return document.createElement(element);
 }
@@ -27,6 +40,7 @@ function hideSubjects(){
 function courseSearch() {
     document.getElementById("courseSearchResults").innerHTML = "";
     var inputVal = document.getElementById("courseCodeInput").value;
+    inputVal = sanitize(inputVal);
 	const ul = document.getElementById("courseSearchResults");
 	fetch("/api/getCourseCodes/" + inputVal)
 		.then((resp) => resp.json())
@@ -40,5 +54,127 @@ function courseSearch() {
     })
 }
 
-// Error catching for fetches in front end
-// Timetable search, show all courses, search by subject code
+function timetableSearch() {
+    document.getElementById("timetableSearch").innerHTML = "";
+    var SC = document.getElementById("timetableSubject").value;
+    var CC = document.getElementById("timetableCourseCode").value;
+    var OC = document.getElementById("timetableCourseComponent").value;
+    const ul = document.getElementById("timetableSearch");
+    SC = sanitize(SC);
+    CC = sanitize(CC);
+    OC = sanitize(OC);
+	fetch("/api/getCourseSearch/" + SC + "/" + String(CC) + "/1")
+		.then((resp) => resp.json())
+		.then(data => {
+            console.log(data);
+            if (data.length == 0){
+                ul.appendChild(document.createElement('li').appendChild(document.createTextNode(`Error - No matchs found. Please check input`)));
+            }
+            else{
+            data.forEach(e => {
+                ul.appendChild(document.createElement('li').appendChild(document.createTextNode(`Subject: ${e.subject}`)));
+                ul.appendChild(document.createElement('p'));
+                ul.appendChild(document.createElement('li').appendChild(document.createTextNode(`Class name: ${e.className}`)));
+                ul.appendChild(document.createElement('p'));
+                ul.appendChild(document.createElement('li').appendChild(document.createTextNode(`Class Number: ${e.catalog_nbr}`)));
+                ul.appendChild(document.createElement('p'));
+                ul.appendChild(document.createElement('li').appendChild(document.createTextNode(`Course Code: ${e.catalog_nbr}`)));
+                ul.appendChild(document.createElement('p'));
+                ul.appendChild(document.createElement('li').appendChild(document.createTextNode(`Description: ${e.catalog_description}`)));
+            });
+        }
+    })
+}
+
+function createSchedule() {
+    var scheduleName = document.getElementById("scheduleName").value;
+    scheduleName = sanitize(scheduleName);
+    console.log(scheduleName);
+    if (scheduleName.length == 0){
+        console.log("No input");
+    }
+    else{
+    fetch("/api/createSchedule/" + scheduleName , {method: 'POST'})
+        .then((resp) => resp.json())
+        .then(data => {
+            console.log(data);
+        })
+    }
+}
+
+function viewSchedules() {
+    document.getElementById("viewSchedules").innerHTML = "";
+    const ul = document.getElementById("viewSchedules");
+    fetch("/api/viewSchedules")
+        .then((resp) => resp.json())
+		.then(data => {
+            console.log(data);
+            Object.keys(data).map(function(key, index) {
+                let content = 'Schedule Name: ' + key + "  || Number of courses: " + Object.keys(data).length;
+                const item = document.createElement('li');
+                item.appendChild(document.createTextNode(content));
+                ul.appendChild(item);
+            });
+        })
+        .catch()
+}
+
+function deleteSchedule() {
+    let toBeDeleted = document.getElementById("scheduleToDelete").value;
+    toBeDeleted = sanitize(toBeDeleted);
+    if (toBeDeleted.length == 0){
+        console.log("No input");
+    }
+    else{
+        fetch("/api/deleteSchedule/" + toBeDeleted, {method: 'DELETE'})
+            .then(data => {
+                console.log("Schedule: " + toBeDeleted + " deleted");
+        })
+    }
+}
+
+function deleteAllSchedules() {
+    fetch("/api/deleteAllSchedules", {method: 'DELETE'})
+		.then((resp) => resp.json())
+		.then(data => {
+            console.log("Schedules all deleted");
+    })
+}
+
+function addCourses() {
+    let scheduleName = document.getElementById("updateScheduleName").value;
+    let courses = {};
+    for(var i = 1; i < 7; i++){
+        var key = sanitize(document.getElementById(String(i) + "c").value);
+        var val = sanitize(document.getElementById(String(i) + "n").value);
+        if (key != "" && val != ""){
+            courses[key] = val;
+        }
+    }
+    console.log(courses);
+    fetch("/api/updateSchedule/" + scheduleName, {method: 'PUT', body: JSON.stringify(courses), headers: { 'Content-Type': 'application/json' }})
+		.then(data => {
+            console.log("Schedules are updated");
+    })
+}
+
+function showSchedule() {
+    document.getElementById("showSchedule").innerHTML = "";
+    let scheduleName = document.getElementById("showScheduleCourses").value;
+    if (scheduleName.length == 0){
+        console.log("No input");
+    } else{
+        fetch("/api/getSchedule/" + scheduleName)
+            .then((resp) => resp.json())
+            .then(data => {
+                console.log(data);
+                const ul = document.getElementById("showSchedule");
+                Object.keys(data).map(function(key, index) {
+                    let content = 'Course Code: ' + key + " Subject Code: " + data[key];
+                    const item = document.createElement('li');
+                    item.appendChild(document.createTextNode(content));
+                    ul.appendChild(item);
+                });
+            });
+    }
+}
